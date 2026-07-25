57ba787c7d88d46205a82a742f503d8d3a2c3de3
RaniaAkramm <ssess535@gmail.com>
2026-07-25 16:52:58 +0300

    Implement category page to display products

---
+++ app/category/[slug]/page.tsx
@@ -0,0 +1,59 @@
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import ProductCard from "@/components/ProductCard";
+import { products } from "@/data/products";
+import { notFound } from "next/navigation";
+
+type PageProps = {
+  params: Promise<{
+    slug: string;
+  }>;
+};
+
+export default async function CategoryPage({ params }: PageProps) {
+  const { slug } = await params;
+
+  const category = slug.replace(/-/g, " ").toLowerCase();
+
+  const categoryProducts = products.filter(
+    (product) =>
+      product.category.toLowerCase() === category
+  );
+
+  if (categoryProducts.length === 0) {
+    notFound();
+  }
+
+  return (
+    <>
+      <Header />
+
+      <main className="section">
+        <div className="container">
+
+          <h1 className="title">
+            {slug.replace(/-/g, " ")}
+          </h1>
+
+          <div
+            className="grid grid-4"
+            style={{ marginTop: "40px" }}
+          >
+            {categoryProducts.map((product) => (
+              <ProductCard
+                key={product.id}
+                id={product.id}
+                name={product.name}
+                price={`$${product.price.toFixed(2)}`}
+                image={product.image}
+              />
+            ))}
+          </div>
+
+        </div>
+      </main>
+
+      <Footer />
+    </>
+  );
+}
