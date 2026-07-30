ab2e5832cf1d7773c0f8e2fc1f48a99ece3996ee
RaniaAkramm <ssess535@gmail.com>
2026-07-30 22:43:59 +0300

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,1 +1,14 @@
+import type { MetadataRoute } from "next";
 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    sitemap: "https://lividshop.com/sitemap.xml",
+    host: "https://lividshop.com",
+  };
+}
